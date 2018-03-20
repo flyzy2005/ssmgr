e9a04db6ab31df3b51deba1e7a5b275c25c5b150
flyzy2005 <flyzy2005@gmail.com>
2018-03-20 01:25:32 +0000

    添加centos系统 m端tiny版一键脚本

---
+++ s-centos-tiny.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+# Copyright (c) 2018 flyzy2005
+
+install() {
+	service iptables stop
+	yum install python-setuptools && easy_install pip
+	yum install git
+	pip install git+https://github.com/shadowsocks/shadowsocks.git@master
+	ssserver -m aes-256-cfb -p 1001 -k aqwesdfas --manager-address 127.0.0.1:6001 -d start
+	git clone https://github.com/gyteng/shadowsocks-manager-tiny.git
+	curl -sL https://rpm.nodesource.com/setup_6.x | bash -
+	yum install -y nodejs
+	screen -dmS ssmgr node shadowsocks-manager-tiny/index.js 127.0.0.1:6001 0.0.0.0:4001 $1
+}
+
+
+if [ "$EUID" -ne 0 ]; then
+	echo '必需以root身份运行，请使用sudo命令'
+	exit 1;
+fi
+
+
+case $1 in 
+	-i )
+	install $2
+	;;
+	* )
+	echo 'm-centos-tiny Version 1.0, 2018-01-20, Copyright (c) 2018 flyzy2005'
+	exit 0;
+	;;
+esac
